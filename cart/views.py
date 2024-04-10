150b9c015ae39e9165f775668e9bdd24cdca8908
jamesxpower <jamesxpower@gmail.com>
2024-04-10 13:45:04 +0800

    4/10 study commit

--- cart/views.py
+++ cart/views.py
@@ -9,7 +9,7 @@ from .forms import CartAddProductForm
 def cart_add(request, product_id):
     cart = Cart(request=request)
     product = get_object_or_404(Product, id=product_id)
-    form = CartAddProductForm(requests.POST)
+    form = CartAddProductForm(request.POST)
 
     if form.is_valid():
         cd = form.cleaned_data
@@ -18,3 +18,24 @@ def cart_add(request, product_id):
                  override_quantity=cd['override'])
 
     return redirect('cart:cart_detail')
+
+
+@require_POST
+def cart_remove(request, product_id):
+    cart = Cart(request=request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+
+    return redirect('cart:cart_detail')
+
+
+def cart_detail(request):
+    cart = Cart(request=request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(
+                                            initial={'quantity': item['quantity'],
+                                                     'override': True})
+
+    return render(request=request,
+                  template_name='cart/detail.html',
+                  context={'cart': cart})
